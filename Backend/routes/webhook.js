84a3282c68200a16d5f162cb98a57265ad4aef17
HuzaifaAslam99 <huzaifaaslam322@gmail.com>
2026-04-20 16:45:23 -0400

    small update

--- Backend/routes/webhook.js
+++ Backend/routes/webhook.js
@@ -30,18 +30,18 @@ router.post("/webhook", async (req, res) => {
 
     // 4. Update MongoDB
     const Order = req.app.locals.Order;
-    const updatedOrder = await Order.findOneAndUpdate(
-      { orderId: orderId },
-      { status: "Paid" },
-      { returnDocument: 'after' } // Changed from { new: true }
-    );
-    ////
-
     // const updatedOrder = await Order.findOneAndUpdate(
     //   { orderId: orderId },
     //   { status: "Paid" },
-    //   { new: true }
+    //   { returnDocument: 'after' } // Changed from { new: true }
     // );
+    ////
+
+    const updatedOrder = await Order.findOneAndUpdate(
+      { orderId: orderId },
+      { status: "Paid" },
+      { new: true }
+    );
 
     if (!updatedOrder) {
       console.log("Order ID not found in DB:", orderId);
