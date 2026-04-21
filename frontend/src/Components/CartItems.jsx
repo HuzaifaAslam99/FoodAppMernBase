52b1802c5f07bf336d4532547af1f85ffe28da64
HuzaifaAslam99 <huzaifaaslam322@gmail.com>
2026-04-21 03:44:26 -0400

    small update

--- frontend/src/Components/CartItems.jsx
+++ frontend/src/Components/CartItems.jsx
@@ -161,9 +161,9 @@ const ERC20_ABI = [
         // setMessage("Confirming ETH Payment...");
         setProcessingMessage("Confirming ETH Payment...")
 
-        tx = await contract.payForOrder(orderId, 0, 0, { value: amountEthWei });
+        // tx = await contract.payForOrder(orderId, 0, 0, { value: amountEthWei });
         // Change this line in your ETH flow:
-        // tx = await contract.payForOrder(orderId, 0, amountEthWei, { value: amountEthWei, gasLimit: 300000 });
+        tx = await contract.payForOrder(orderId, 0, amountEthWei, { value: amountEthWei, gasLimit: 300000 });
       }
 
       await tx.wait();
