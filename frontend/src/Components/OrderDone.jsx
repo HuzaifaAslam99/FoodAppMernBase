352890a7fe7c816d993bbf6524e884f7ee136698
HuzaifaAslam99 <huzaifaaslam322@gmail.com>
2026-04-23 15:35:14 -0400

    small update

--- frontend/src/Components/OrderDone.jsx
+++ frontend/src/Components/OrderDone.jsx
@@ -20,3 +20,14 @@ function OrderDone() {
 }
 
 export default OrderDone
+
+
+
+
+
+
+
+fetch('https://gateway.pinata.cloud/ipfs/bafkreihfcl2gqo7grhisj4bajzpdh4yufwjenwd6yaplugj65oxkaxczty')
+  .then(res => res.text())
+  .then(data => console.log("The 'Normal' Data is:", data))
+  .catch(err => console.error("Error fetching IPFS data:", err));
